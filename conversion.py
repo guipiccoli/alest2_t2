5cc904606e336f46989da4e66b2a90d242c9d6bd
Guilherme Piccoli <guuipiccoli@gmail.com>
2018-10-11 16:44:29 -0300

    entrada e saida

--- conversion.py
+++ conversion.py
@@ -1,33 +1,35 @@
 def hex2bin(hex):
     if hex == '0':
         return '0000'
-    if hex == '1':
+    elif hex == '1':
         return '0001'
-    if hex == '2':
+    elif hex == '2':
         return '0010'
-    if hex == '3':
+    elif hex == '3':
         return '0011'
-    if hex == '4':
+    elif hex == '4':
         return '0100'
-    if hex == '5':
+    elif hex == '5':
         return '0101'
-    if hex == '6':
+    elif hex == '6':
         return '0110'
-    if hex == '7':
+    elif hex == '7':
         return '0111'
-    if hex == '8':
+    elif hex == '8':
         return '1000'
-    if hex == '9':
+    elif hex == '9':
         return '1001'
-    if hex == 'a' or 'A':
+    elif hex == 'a' or 'A':
         return '1010'
-    if hex == 'b' or 'B':
+    elif hex == 'b' or 'B':
         return '1011'
-    if hex == 'c' or 'C':
+    elif hex == 'c' or 'C':
         return '1100'
-    if hex == 'd' or 'D':
+    elif hex == 'd' or 'D':
         return '1101'
-    if hex == 'e' or 'E':
+    elif hex == 'e' or 'E':
         return '1110'
-    if hex == 'f' or 'F':
+    elif hex == 'f' or 'F':
         return '1111'
+    else:
+        return 'erro'
--- labirinto.py
+++ labirinto.py
@@ -4,28 +4,75 @@ import conversion as cvs
 #Direita: Sem parede
 #Inferior: Parede
 #Esquerda: Sem parede
+class Node:
+    def __init__(self,superior,direita,inferior,esquerda):
+        self.superior = superior
+        self.direita = direita
+        self.inferior = inferior
+        self.esquerda = esquerda
+
+entrada = None
+saida = None
 
 def labirinto(list):
+    global entrada
+    global saida
     for i in range(len(list)):
-        if i == 0:
-            for elem in list[i]:
-                if elem.find('0') == 0:
-                    print(elem)
-                    
-        if i == len(list)-1:
-            for elem in list[i]:
-                if elem.find('0') == 2:
-                    print(elem)
+        for elem in list[i]:
+            if i == 0:
+                Node(elem[0],elem[1],elem[2],elem[3])
+                if elem[0] == '0':
+                    entrada = elem
+
+            elif i == len(list)-1:
+                for elem in list[i]:
+                    if elem[2] == '0':
+                        saida = elem
+
+            elif elem[3] == '0' and list[0] or (list[len(list)-1] and elem[1] == '0'):
+                if entrada == None:
+                    entrada = elem
+                else:
+                    saida = elem
+    print(entrada)
+    print(saida)
+
+def caminha(node):
+    if node == entrada2:
+        return 1
+    node.flag = 1
+
+    if node.cima != '1' and node.cima.flag == 0:
+        res = caminha(node.cima)
+        if res > 0:
+            return 1 + res
+    if node.direita != '1' and node.direita.flag == 0:
+        res = caminha(node.direita)
+        if res > 0:
+            return 1 + res
+    if node.baixo != '1' and node.baixo.flag == 0:
+        res = caminha(node.baixo)
+        if res > 0:
+            return 1 + res
+    if node.esquerda != '1' and node.esquerda.flag == 0:
+        res = caminha(node.esquerda)
+        if res > 0:
+            return 1 + res
+
+    node.flag = 2
+    return 0
 
 def main ():
-    f = open('t2-casos/caso500a.txt', 'r')
+    f = open('t2-casos/teste.txt', 'r')
     data = f.readlines()
     elem = []
     elementos = []
     binary_string =[]
     nm = data[0].split(' ')[0]
     for i in range (1,(int(nm)+1)):
+        data[i] = data[i].strip('\n')
         elem.append(data[i].split(' '))
-        binary_string.append(list(map(lambda x:cvs.hex2bin(x),elem[i-1][:-1])))
+        elementos.append(elem[i-1])
+        binary_string.append(list(map(lambda x:cvs.hex2bin(x),elem[i-1])))
     labirinto(binary_string)
 if __name__ == "__main__": main()
